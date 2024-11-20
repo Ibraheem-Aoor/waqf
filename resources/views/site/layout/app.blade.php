e2d8375a20d5593d598ae9fd3c06ce864afe1a26
Ibraheem <ibraheem.alaoor@hotmail.com>
2024-11-21 01:27:21 +0200

    up

--- resources/views/site/layout/app.blade.php
+++ resources/views/site/layout/app.blade.php
@@ -36,7 +36,6 @@
 
     <link rel="stylesheet" href="{{ asset('css/site/bootstrap.min.css') }}" />
 
-    <link rel="stylesheet" href="{{ asset('css/site/main.css') }}" />
 
     <style>
         :root {
@@ -44,7 +43,7 @@
         }
 
         .base_color {
-            color:#fff !important;
+            color: #fff !important;
             background-color: {{ $color }};
         }
 
@@ -58,6 +57,8 @@
             fill: {{ $color }};
         }
     </style>
+    <link rel="stylesheet" href="{{ asset('css/site/main.css') }}?v=0.01" />
+
 
     @stack('css')
 </head>
@@ -84,7 +85,7 @@
                 <ul class="menu">
                     <li>
                         <a class="{{ areActiveRoutes(['site.home']) }}" href="{{ route('site.home') }}">
-                         <img src="{{ asset('images/home.png') }}" alt="home" width="24">
+                            <img src="{{ asset('images/home.png') }}" alt="home" width="24">
 
 
 
@@ -181,11 +182,35 @@
         <header class="main-header">
             <div class="container">
                 <div class="row align-items-center">
+                    @if (Route::currentRouteName() != 'site.home')
+                        <div class="col-2">
+                            <a class="btn-icon" href="{{ url()->previous() }}">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                    fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd"
+                                        d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                                        fill="#000"></path>
+                                </svg>
+                            </a>
+                        </div>
+                    @endif
                     <div class="col-8 mx-auto">
                         <div class="text-center logo">
-                            <img src="https://waqfsawir.net/img/logo.jpeg" alt="">
+                            <img src="https://waqfsawir.net/img/logo.jpeg" alt="" />
                         </div>
                     </div>
+                    @if (Route::currentRouteName() != 'site.home')
+                        <div class="col-2">
+                            <a class="btn-icon me-auto" href="{{ route('site.home') }}">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" height="18"
+                                    width="18">
+                                    <path
+                                        d="M 16 2.59375 L 15.28125 3.28125 L 2.28125 16.28125 L 3.71875 17.71875 L 5 16.4375 L 5 28 L 14 28 L 14 18 L 18 18 L 18 28 L 27 28 L 27 16.4375 L 28.28125 17.71875 L 29.71875 16.28125 L 16.71875 3.28125 Z M 16 5.4375 L 25 14.4375 L 25 26 L 20 26 L 20 16 L 12 16 L 12 26 L 7 26 L 7 14.4375 Z">
+                                    </path>
+                                </svg>
+                            </a>
+                        </div>
+                    @endif
                 </div>
             </div>
         </header>
